395f7481a912d174ce61b7f07d64d396f89ba030
passhold16-web <passhold16@gmail.com>
2026-05-10 11:59:41 +0200

    Enable Service Worker registration in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { registerServiceWorker } from "./registerSW";
+
+// Registrar Service Worker
+registerServiceWorker();
 
 createRoot(document.getElementById("root")!).render(<App />);
